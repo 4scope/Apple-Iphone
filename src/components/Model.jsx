ad78a18a925a73d9e4526c2f62cc0d5545889f40
4Scope <agent4scope@gmail.com>
2024-08-09 20:13:54 +0100

    3d Model logic

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,36 +1,70 @@
-import ModelView from "../components/ModelView";
-import * as THREE from "three";
-import { useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
-import gsap from "gsap/all";
+import gsap from "gsap";
+import ModelView from "./ModelView";
+import { useEffect, useRef, useState } from "react";
 import { yellowImg } from "../utils";
+
+import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
 import { models, sizes } from "../constants";
+import { animateWithGsapTimeline } from "../utils/animation";
 
 const Model = () => {
-	useGSAP(() => {
-		gsap.to("#heading", { y: 0, opacity: 1, delay: 1 });
-	}, []);
-
 	const [size, setSize] = useState("small");
 	const [model, setModel] = useState({
-		title: "IPhone 15 Pro in Natural Titanium",
+		title: "iPhone 15 Pro in Natural Titanium",
 		color: ["#8F8A81", "#FFE7B9", "#6F6C64"],
 		img: yellowImg,
 	});
 
+	// camera control for the model view
 	const cameraControlSmall = useRef();
 	const cameraControlLarge = useRef();
 
+	// model
 	const small = useRef(new THREE.Group());
 	const large = useRef(new THREE.Group());
 
-	const [smallRotation, setSmallRotation] = useState();
-	const [largeRotation, setLargeRotation] = useState();
+	// rotation
+	const [smallRotation, setSmallRotation] = useState(0);
+	const [largeRotation, setLargeRotation] = useState(0);
 
-	smallRotation;
-	largeRotation;
+	const tl = gsap.timeline();
+
+	useEffect(() => {
+		if (size === "large") {
+			animateWithGsapTimeline(
+				tl,
+				small,
+				smallRotation,
+				"#view1",
+				"#view2",
+				{
+					transform: "translateX(-100%)",
+					duration: 2,
+				}
+			);
+		}
+
+		if (size === "small") {
+			animateWithGsapTimeline(
+				tl,
+				large,
+				largeRotation,
+				"#view2",
+				"#view1",
+				{
+					transform: "translateX(0)",
+					duration: 2,
+				}
+			);
+		}
+	}, [size]);
+
+	useGSAP(() => {
+		gsap.to("#heading", { y: 0, opacity: 1 });
+	}, []);
 
 	return (
 		<section className='common-padding'>
@@ -38,21 +72,23 @@ const Model = () => {
 				<h1 id='heading' className='section-heading'>
 					Take a closer look.
 				</h1>
+
 				<div className='flex flex-col items-center mt-5'>
-					<div className='w-full h-[75vh] md:has-[90vh] overflow-hidden relative'>
+					<div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
 						<ModelView
 							index={1}
 							groupRef={small}
-							gaspType='view1'
+							gsapType='view1'
 							controlRef={cameraControlSmall}
 							setRotationState={setSmallRotation}
 							item={model}
 							size={size}
 						/>
+
 						<ModelView
 							index={2}
 							groupRef={large}
-							gaspType='view2'
+							gsapType='view2'
 							controlRef={cameraControlLarge}
 							setRotationState={setLargeRotation}
 							item={model}
@@ -82,18 +118,16 @@ const Model = () => {
 
 						<div className='flex-center'>
 							<ul className='color-container'>
-								{models.map((item, i) => {
-									return (
-										<li
-											key={i}
-											className='w-6 h-6 rounded-full mx-2 cursor-pointer'
-											style={{
-												backgroundColor: item.color[0],
-											}}
-											onClick={() => setModel(item)}
-										/>
-									);
-								})}
+								{models.map((item, i) => (
+									<li
+										key={i}
+										className='w-6 h-6 rounded-full mx-2 cursor-pointer'
+										style={{
+											backgroundColor: item.color[0],
+										}}
+										onClick={() => setModel(item)}
+									/>
+								))}
 							</ul>
 
 							<button className='size-btn-container'>
@@ -105,7 +139,7 @@ const Model = () => {
 											backgroundColor:
 												size === value
 													? "white"
-													: "black",
+													: "transparent",
 											color:
 												size === value
 													? "black"
