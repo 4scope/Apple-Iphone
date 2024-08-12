82fceac252ad11cd009292e49878b0fcda6f9a07
4Scope <agent4scope@gmail.com>
2024-08-12 18:26:56 +0100

    feature animations

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,13 +2,29 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import React, { useRef } from "react";
 import { animateWithGsap } from "../utils/animation";
-import { explore1Img, exploreVideo } from "../utils";
+import { explore1Img, explore2Img, exploreVideo } from "../utils";
 
 const Feature = () => {
 	const videoRef = useRef();
 
 	useGSAP(() => {
+		gsap.to("#exploreVideo", {
+			scrollTrigger: {
+				trigger: "#exploreVideo",
+				toggleActions: "play pause reverse restart",
+				start: "-10% bottom",
+			},
+			onComplete: () => {
+				videoRef.current.play();
+			},
+		});
 		animateWithGsap("#feature_title", { opacity: 1, y: 0 });
+		animateWithGsap(
+			".g_grow",
+			{ scale: 1, opacity: 1, ease: "power1" },
+			{ scrub: 5.5 }
+		);
+		animateWithGsap(".g_text", { opacity: 1, y: 0, ease: "power1" });
 	}, []);
 
 	return (
@@ -50,6 +66,39 @@ const Feature = () => {
 										alt='titanium'
 										className='feature-video g_grow'
 									/>
+								</div>
+								<div className='overflow-hidden flex-1 h-[50vh]'>
+									<img
+										src={explore2Img}
+										alt='titanium 2'
+										className='feature-video g_grow'
+									/>
+								</div>
+							</div>
+
+							<div className='feature-text-container'>
+								<div className='flex-1 flex-center'>
+									<p className='feature-text g_text'>
+										iPhone 15 pro{" "}
+										<span className='text-white'>
+											the first iPhone to feature an
+											aerospace-grade titanium design
+										</span>
+										, using the same alloy that spacecrafts
+										use for missions to Mars.
+									</p>
+								</div>
+								<div className='flex-1 flex-center'>
+									<p className='feature-text g_text'>
+										Titanium has one of the bast
+										strength-to-weight ratios of any metal,
+										making these our{" "}
+										<span className='text-white'>
+											lightest Pro models ever.
+										</span>
+										You'll notice the difference the moment
+										you pick one up
+									</p>
 								</div>
 							</div>
 						</div>
